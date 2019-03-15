8b3c7f1c85755836d1a199ba64ebad1d4d5265ad
huyennvuu <vukhanhhuyen99@gmail.com>
2019-03-15 16:11:03 +0700

    Create a GUI represents the add function

---
+++ ToDos/src/main/java/ListGUI.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ListGUI extends JFrame implements ActionListener {
+
+    // make a box to add new plan
+    private String textBox;
+
+    ListGUI(String title){
+        super(title);
+
+        //create "add" button.
+        JButton addButton=new JButton(new ImageIcon("C:\\Users\\HP\\Desktop\\add.png"));
+        addButton.setActionCommand("Add");
+        addButton.addActionListener(this);
+        addButton.setBounds(15,10,25,25);
+        addButton.setBorder(BorderFactory.createEmptyBorder());
+        addButton.setContentAreaFilled(false);
+        addButton.setFocusPainted(false);
+
+        // add function for button
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                textBox=JOptionPane.showInputDialog("enter the new plan: ");
+                JOptionPane.showMessageDialog(null,textBox);
+                System.out.print(textBox);
+            }
+        });
+
+
+        //add "add" Button into frame
+        this.add(addButton);
+        this.setSize(300,400);
+        this.setLayout(null);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+
+    }
+    public static void main(String[] args) {
+        new ListGUI("Todo List");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        // do somethings
+    }
+}
--- ToDos/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Task myTask = new Task();
-        myTask.addContent();
-        System.out.println(myTask.getContent());
-        //List myList = new List();
-        //myList.addTask(myTask);
-    }
-}
