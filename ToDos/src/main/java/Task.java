198e3246019af2b685dfd58d115f6f4bc7cb645f
MidoDoan <huongdl.hx@gmail.com>
2019-03-17 19:44:52 +0700

    Update Task class with method printContent

--- ToDos/src/main/java/Task.java
+++ ToDos/src/main/java/Task.java
@@ -1,30 +1,67 @@
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Task {
     private String content;
-    private Date dueDate;
-    public Task(){
-
-    }
-    public boolean addContent(String newContent){
+    private Calendar dueDate;
+    private String state;
+    public Task(){}
+    //add new content
+    public void addContent(String newContent){
         this.content = newContent;
-        return true;
+        //return true;
     }
+    public void addContent() {
+        Scanner scan = new Scanner(System.in);
+        content = scan.nextLine();
+    }
+    //get content
     public String getContent(){
         return this.content;
     }
 
-    public boolean addContent() {
-        Scanner scan = new Scanner(System.in);
-        content = scan.nextLine();
-        return true;
+    //print content in console
+    public void printContent(){
+        System.out.println(this.content);
     }
-    /*public boolean addDueDate(int year, int month, int date){
-        dueDate = new Date(year, month, date);
-        return true;
 
-    }*/
+    //set state for task
+    public void setState(String currentstate){
+        this.state = currentstate;
+    }
 
+    //set duedate
+    public void setDueDate(int year,int month,int date){
+        month=month-1;
+        this.dueDate = Calendar.getInstance();
+        this.dueDate.set(year,month,date);
+        this.dueDate.set(Calendar.HOUR,0);
+        this.dueDate.set(Calendar.MINUTE,0);
+        this.dueDate.set(Calendar.MILLISECOND,0);
+    }
+
+    //get DueDate
+    public String getDueDate(){
+        return this.dueDate.getTime().toString();
+    }
+
+    //set state
+    public void setState(){
+        Calendar today = Calendar.getInstance();
+        if(today.compareTo(this.dueDate)>0){    //dueDate is after today
+            this.state ="Overdue";
+        }
+        else if (today.compareTo(this.dueDate)<0){  //dueDate is before today
+            this.state ="Not done";
+        }
+        //else this.state="done";
+    }
+
+    // get task's state
+    public String getState(){
+        return this.state;
+    }
 
 }
+
