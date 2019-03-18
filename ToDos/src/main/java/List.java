dd109e3cbf9a469634406ebcdf2e0f2029312dcb
linhhonblade <meomaimaimeo@gmail.com>
2019-03-18 18:48:45 +0700

    adding simple implementation of List class

--- ToDos/src/main/java/List.java
+++ ToDos/src/main/java/List.java
@@ -1,18 +1,18 @@
 import java.util.ArrayList;
 
-public class List{
-    private ArrayList<Task> taskList;
-
-    public List(){
-        taskList = new ArrayList<Task>();
-    }
-    public void addTask(Task newTask){
-        if(newTask==null){
-            System.out.println("Wrong input format, should be an instance of class Task");
-        }
-        else{
-            taskList.add(newTask);
-            System.out.println("New task added");
+public class List extends ArrayList<Task> {
+    public void showList(){
+        System.out.println("List Info:");
+        for(int i = 0; i < this.size(); i++){
+            this.get(i).printContent();
         }
     }
+    /*public static void main(String[] args){
+        List myList = new List();
+        *//*Task myTask1 = new Task("Buy milk");
+        Task myTask2 = new Task("Wash clothes");*//*
+        myList.add(new Task("Buy milk"));
+        myList.add(new Task("Wash clothes"));
+        myList.showList();
+    }*/
 }
