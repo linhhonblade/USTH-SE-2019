17ea3785545af0174e9469e21382fec39759f2d7
linhhonblade <meomaimaimeo@gmail.com>
2019-03-22 15:26:45 +0700

    update Main

--- ToDos/src/main/java/Main.java
+++ ToDos/src/main/java/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.SwingUtilities;
+
 public class Main{
   public static void main(String[] args){
-    ListGUI1 newListGUI = new ListGUI1("Todo List");
+    SwingUtilities.invokeLater(new Runnable(){
+      public void run()
+      {
+        ListGUI1 newListGUI = new ListGUI1("Todo List");
+      }
+    });
   }
 }
