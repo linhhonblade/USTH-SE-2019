e66362ff25e3e82ca5f203ee761ab79218bfbb34
linhhonblade <meomaimaimeo@gmail.com>
2019-04-02 14:09:21 +0700

    add Demo

---
+++ ToDos/src/main/java/Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args){
+        List myList = new List();
+        myList.add(new Task("hello"));
+        myList.add(new Task("dhfwefh"));
+        View view = new View(myList);
+        Controller controller = new Controller(myList, view);
+        controller.setListener();
+    }
+}
