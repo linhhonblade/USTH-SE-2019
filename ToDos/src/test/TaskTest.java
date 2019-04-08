8f0d2fb2d412996b8b7b788a6355a8ce9a927d8d
MidoDoan <huongdl.hx@gmail.com>
2019-04-08 19:21:06 +0700

    Add testTask class

---
+++ ToDos/src/test/TaskTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class TaskTest {
+    Task myTask = new Task("hello");
+    @Before
+    public void init(){
+        myTask.setState("done");
+    }
+    @Test
+    public void testgetContent(){
+        assertEquals("hello",myTask.getContent());
+    }
+
+    @Test
+    public void testprintContent() throws Exception{
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        myTask.printContent();
+        String expectedOutput = "hello\n";
+        assertEquals(expectedOutput,outContent.toString());
+    }
+    @Test
+    public void testgetState(){
+        assertEquals("done",myTask.getState());
+    }
+
+}
