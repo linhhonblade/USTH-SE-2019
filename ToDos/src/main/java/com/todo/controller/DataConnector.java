6c720a8a0be458052c7b378a711da7ee3d3b2d16
linhhonblade <meomaimaimeo@gmail.com>
2019-04-16 07:59:45 +0700

    adding data controller object

---
+++ ToDos/src/main/java/com/todo/controller/DataConnector.java
@@ -0,0 +1,60 @@
+package com.todo.controller;
+
+import com.todo.model.List;
+import com.todo.model.Task;
+
+import java.sql.*;
+
+
+public class DataConnector{
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "org.h2.Driver";
+    static final String DB_URL = "jdbc:h2:~/test";
+
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+    private Connection connect = null;
+    private Statement statement = null;
+
+    //  Database credentials
+    static final String USER = "sa";
+    static final String PASS = "";
+
+    public List readData() throws SQLException, ClassNotFoundException {
+        List list = new List();
+        Class.forName(JDBC_DRIVER);
+        System.out.println("Connecting to database...");
+        connect = DriverManager.getConnection(DB_URL,USER,PASS);
+        System.out.println("Connected database successfully...");
+
+        statement = connect.createStatement();
+        String sql = "SELECT * FROM Todolist";
+        resultSet = statement.executeQuery(sql);
+
+        while(resultSet.next()){
+            Task task = new Task(resultSet.getString("content"));
+            list.add(task);
+        }
+
+        resultSet.close();
+        return list;
+    }
+
+    public void writeData(List list) throws SQLException, ClassNotFoundException{
+        Class.forName(JDBC_DRIVER);
+        System.out.println("Connecting to database...");
+        connect = DriverManager.getConnection(DB_URL,USER,PASS);
+        System.out.println("Connected database successfully...");
+
+        String sql = "DELETE FROM Todolist";
+        preparedStatement = connect.prepareStatement(sql);
+        preparedStatement.executeUpdate();
+
+        for(Task task: list){
+            sql = "INSERT INTO Todolist " +  "VALUES(\'" + task.getContent() + "\')";
+            System.out.println(sql);
+            preparedStatement = connect.prepareStatement(sql);
+            preparedStatement.executeUpdate();
+        }
+    }
+}
